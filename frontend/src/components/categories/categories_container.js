07fda1e9da2f9884497ff435bdb5c2c73a2182b7
Brandon Leong <bjtl1105@gmail.com>
2020-10-29 09:30:29 -0700

    category fixes

--- frontend/src/components/categories/categories_container.js
+++ frontend/src/components/categories/categories_container.js
@@ -14,4 +14,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Categories);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoriesIndex);
--- frontend/src/reducers/categories_reducer.js
+++ frontend/src/reducers/categories_reducer.js
@@ -2,6 +2,7 @@ import { RECEIVE_CATEGORIES } from '../actions/categories_actions';
 
 const CategoriesReducer = (state = {}, action) => {
     Object.freeze(state)
+    let newState = Object.assign({}, state);
     switch(action.type){
         case RECEIVE_CATEGORIES:
             newState = action.categories
