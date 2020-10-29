0ceeb3b99e18d35f71832059998c5a162206c1be
Brandon Leong <bjtl1105@gmail.com>
2020-10-28 17:32:47 -0700

    cardReducer

---
+++ frontend/src/reducers/cards_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CARDS, RECEIVE_CARD, RECEIVE_USER_CARDS} from '../actions/card_actions';
+
+const CardsReducer = (state = { all: {}, user: {}, user_cards: {} }, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVE_CARDS:
+            newState.all = action.cards.data;
+            return newState;
+        case RECEIVE_CARD:
+            newState.user = action.cards.data;
+            return newState;
+        case RECEIVER_USER_CARDS:
+            newState.user_cards = actions.cards.data
+            return newState;      
+        default:
+            return state;
+    }
+};
+
+export default CardsReducer;
