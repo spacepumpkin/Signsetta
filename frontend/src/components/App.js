fcca191c32e8b38de7d414bd2a8db674608ff6c1
Malcolm Reyes <mreyesx99@gmail.com>
2020-10-29 23:29:22 -0700

    semantic-ui-setu-b

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,11 +4,11 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container.js';
 // import TweetsContainer from './tweets/tweets_container';
 // import MainPage from './main/main_page';
-import CardIndexContainer from './card/card_index.container'
+import CardIndexContainer from './card/card_index.container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
-import CategoriesIndex from './categories/categories_index_container'
+import CategoriesIndex from './categories/categories_index_container';
 
 // import TweetComposeContainer from './tweets/tweets_compose_container';
 
