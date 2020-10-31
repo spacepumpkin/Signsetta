50b9e2686e868b423cc0a3dd3b23a7d458f2358f
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2020-10-30 17:32:40 -0700

    Add basic semantic ui to splash

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -5,16 +5,23 @@ import SearchContainer from '../search/search_container';
 
 const Splash = (state) => {
     return (
-        <div>
-            <div className="description">SignSetta is an application designed to help 
-            people learn how to use sign language. Users will be able to take classes 
-            and use flashcards to learn sign language and test themselves on various 
-            signs before moving onto more difficult and specific topics in sign language.</div>
+        <div className="ui masthead center aligned segment">
+            <div className="ui text container">
+                <h1 className="ui header teal">
+                    SignSetta is an application designed to help people learn how to use sign language.
+                </h1>
+                <h3>
+                    Users will be able to take classes
+                    and use flashcards to learn sign language and test themselves on various
+                    signs before moving onto more difficult and specific topics in sign language.
+                </h3>
+            </div>
+            <div className="ui divider"></div>
+            <div className="ui category search">
+                <div className="search-bar">Search:<SearchContainer /> </div>
+            </div>
 
-
-            <div className="search-bar">Search:<SearchContainer /> </div>
-
-            <div className="splash-img">
+            <div className="splash-img ui fluid image">
                 <img src={image} className="image" alt="image" />
             </div>
 
