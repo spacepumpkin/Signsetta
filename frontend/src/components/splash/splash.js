4d3d50100771423593c067bbd07d5c837f99dae5
Malcolm Reyes <mreyesx99@gmail.com>
2020-11-11 16:36:37 -0800

    hacking

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -2,33 +2,28 @@ import React from 'react';
 import image from '../../images/splash-image.jpg';
 
 import SearchContainer from '../search/search_bar_container';
-// import { Button, Modal } from 'semantic-ui-react';
-
-// import SignupModal from '../session/signup_form';
-// import "./splash.css";
 
 const Splash = (state) => {
-    // const [open, setOpen] = React.useState(false);
-
     return (
-        <div className="ui masthead center aligned segment">
+        <div className="ui masthead center
+         aligned segment">
             <div className="ui text container">
-                <h1 className="ui header teal">
-                    SignSetta is an application designed to help people learn how to use sign language.
+                <h1 className="ui header teal huge" style={{fontSize: '50px'}}>
+                    Have YOU ever wanted to learn sign language? Start with SignSetta
                 </h1>
                 <h3>
-                    Users will be able to take classes
-                    and use flashcards to learn sign language and test themselves on various
-                    signs before moving onto more difficult and specific topics in sign language.
+                
+                    Use our innovative sign language games and our iconic sign language flashcards
+
                 </h3>
             </div>
             <div className="ui divider"></div>
             <div className="ui category search">
-                <div className="search-bar">Search for any letter or number between 1-10:<SearchContainer /> </div>
+                <div className="search-bar"><SearchContainer /> </div>
             </div>
 
-            <div className="splash-img ui fluid image">
-                <img src={image} className="image" alt="splash" />
+            <div className="splash-img ui fluid image inverted  segment">
+                <img src={image} className="image" alt="splash"  style={{position: 'fixed', zIndex: '-1000', top: '0'}}/>
             </div>
         </div>
 
