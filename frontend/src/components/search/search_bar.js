f5eb554498c64ce2adef75dafdf13561bf59bd47
Isabella <isabella.sandoval95@gmail.com>
2020-11-19 19:52:16 -0700

    splash_updates

--- frontend/src/components/search/search_bar.js
+++ frontend/src/components/search/search_bar.js
@@ -49,8 +49,8 @@ class CategorySearch extends React.Component {
                 </div>
             <div>
 
-                    <div className="ui segment center aligned grid" style={{background: '000000'}}>
-                        <div className="ui centered cards">
+                    <div className="ui segment center aligned grid" style={{background: '000000', border: 'none', boxShadow: 'none'}}>
+                        <div className="ui centered cards" style={{ border: 'none'}}>
 
                             {(returnCard !== undefined) && returnCard.map(card => {
                 
