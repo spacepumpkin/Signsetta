4d44de5ee363188b4a59466a3091ee6176bd2a98
spacepumpkin <68368260+spacepumpkin@users.noreply.github.com>
2021-01-22 14:18:29 -0800

    Change splash hero font. Fix splash image to allow coverage of window regardless of size

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../../images/hands-image.jpg';
+import './splash.css';
 
 // import SearchContainer from '../search/search_bar_container';
 
@@ -8,7 +9,7 @@ const Splash = (state) => {
         <div className="ui masthead center
          aligned segment"style={{ background: '#00000000', border: 'none', boxShadow: 'none' }}>
             <div className="ui text" style={{ background: '000000'}}>
-                <h1 className="ui header teal huge" style={{ fontSize: '70px', textShadow: '2px 2px 15px black', paddingTop: '20%'}} >
+                <h1 id="splash-main-header" className="ui header teal huge" style={{ fontSize: '70px', fontWeight: 'bold', textShadow: '2px 2px 15px black', paddingTop: '20%'}} >
                     Start your ASL journey with Signsetta.
                 </h1>
                 <h3 style={{ color: '#dddddd', textShadow: '2px 2px 10px black', fontSize: '25px' }}>
@@ -21,7 +22,7 @@ const Splash = (state) => {
                 <div className="search-bar"><SearchContainer /> </div>
             </div> */}
 
-            <div className="splash-img ui fluid image  " style={{ background: '#000000', boxShadow: 'none' }}>
+            <div className="splash-img ui fluid image  " style={{ background: '#000000', boxShadow: 'none', height: '100%' }}>
                 <img src={image} className="image" alt="splash" style={{ position: 'fixed', zIndex: '-1000', top: '0', left: '0', filter: 'grayscale(30%) blur(0px)' }} />
             </div>
         </div>
